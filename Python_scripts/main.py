73d964a0a4e46196b6dddf1a726124c70ec18f67
gabe-husain <gabe.husain@gmail.com>
2023-02-13 16:15:49 -0800

    changes G

--- Python_scripts/main.py
+++ Python_scripts/main.py
@@ -11,13 +11,13 @@ for i in range(10):
 
     camera = PiCamera()
     camera.resolution = (3280,2464)
-    #camera.start_preview()
+    camera.start_preview()
     time.sleep(2)
     camera.capture(stream, format='png')
     # "Rewind" the stream to the beginning so we can read its content
     stream.seek(0)
     image = Image.open(stream)
-    image.save("~/HRImaging_SP23/out/img" + str(i) + ".png", "PNG")
+    image.save("../out/img" + str(i) + ".png", "PNG")
     camera.close()
     print("image captured", datetime.datetime.now())
 
